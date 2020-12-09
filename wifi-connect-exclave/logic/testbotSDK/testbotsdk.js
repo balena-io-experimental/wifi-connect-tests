76f78c787b3d6300e1e6f710473967b1b6f41009
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2020-12-10 02:37:28 +0530

    Add testbotSDK setup
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

---
+++ wifi-connect-exclave/logic/testbotSDK/testbotsdk.js
@@ -0,0 +1,34 @@
+// Two approaches 
+// 1. Create testbotSDK object here and flash the image 
+// 2. Use Ryan's server. 
+
+//  Do both. 
+
+// Provision (flash) and Power on DUT (testbot-sdk)
+
+const { RaspberryPi, TestBotHat } = require('@balena/testbot')
+
+
+const testbotHat = new TestBotHat();
+const deviceInteractor = new RaspberryPi(testbotHat);
+
+try {
+	async () => {
+		console.log(`Setting up DUT`)
+		await testbotHat.setup()
+		
+		console.log(`Flashing the DUT`)
+		await deviceInteractor.flashFromFile("/tmp/raspberrypi3.img");
+		
+		console.log(`Powering up DUT`)
+		await testbotHat.setVout(deviceInteractor.powerVoltage);
+		await testbotHat.powerOnDUT();
+		
+		console.log(`Test successful`)
+		await testbotHat.teardown(true)
+		return 0
+	}
+} catch(err) {
+	console.log(`Test Unsuccessful: ${err}`)
+	return 1
+}
