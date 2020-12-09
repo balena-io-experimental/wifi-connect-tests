cc147ef878a8bb8a18214ea9f09fbb30f1d1d4eb
rcooke-warwick <ryan@balena.io>
2020-12-09 21:37:38 +0000

    add testbot server
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- testbot/index.ts
+++ testbot/index.ts
@@ -1,11 +1,19 @@
 //import fs from 'mz';
 //import createGunzip from 'zlib'
 import * as express from "express";
-
+import * as bodyParser from "body-parser";
+import {
+    createAccessPoint,
+    connectToWifi,
+    //getWiFiDevices,
+  } from "./lib/nm";
+  
 
 import { TestBotHat } from './lib/hat';
 
-console.log("starting")
+(async () => {
+
+console.log("Starting testbot service...")
 
 // Create testbotHat instance
 const hatBoard = new TestBotHat();
@@ -13,40 +21,72 @@ hatBoard.setup();
 console.log("Testbot ready...");
 
 
+// Get wifi device ready
+const AP_SSID = "finAP"
+const AP_PASSWORD = "password"
+const iface = "wlan0"
+
+//const _DUT_SSID = "WiFi Connect"
+
+
 const app = express()
+app.use(bodyParser.json());
+
 const port = 8080
+console.log("Server ready...");
 
 app.get('/poweron', async (_req, res) => {
   console.log('Powering on DUT!')
-  await powerOn()
-  res.send('Success')
+  await powerOn(hatBoard);
+  res.send('Ok')
 })
 
 app.get('/poweroff', async (_req, res) => {
     console.log('Powering Off DUT!')
-    await powerOff()
-    res.send('Success')
+    await powerOff(hatBoard);
+    res.send('Ok')
 })
 
-app.get('/flash', (_req, res) => {
+app.get('/flash', async (_req, res) => {
     console.log('Flashing!')
-    res.send('Success')
+    res.send('Ok')
 })
+
+app.get('/createAp', async (_req, res) => {
+    console.log('Creating Access Point!')
+    console.log(`Creating WiFi AP on ${iface} with SSID "${AP_SSID}" and password "${AP_PASSWORD}"...`);
+    await createAccessPoint({ iface: iface, ssid: AP_SSID, password: AP_PASSWORD });
+    res.send('Ok')
+})
+
+app.post('/connectAp', async (req, res) => {
+    console.log('Connecting to Access Point!')
+    console.log(req.body.ssid)
+    if(req.body.ssid != null){
+        console.log(req.body.password)
+        if(req.body.password != null){
+            console.log("Hola")
+            await connectToWifi({ iface: iface, ssid: req.body.ssid, password: req.body.ssid});
+            res.send('Ok')
+        }
+    }
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+})();
 
 
-
-async function powerOn(){
-    await hatBoard.setVout(5);
-	await hatBoard.powerOnDUT();
-	console.log('Vout=', await hatBoard.readVout());
+async function powerOn(hat: TestBotHat){
+    await hat.setVout(5);
+	await hat.powerOnDUT();
+	console.log('Vout=', await hat.readVout());
 }
 
-async function powerOff(){
+async function powerOff(hatBoard: TestBotHat){
     await hatBoard.powerOffDUT();
 }
 
--- wifi-connect-exclave/logic/scripts/quick-test.sh
+++ wifi-connect-exclave/logic/scripts/quick-test.sh
@@ -5,5 +5,10 @@ echo "TEST 0"
 #Ping the endpoint - whats the address though?
 curl http://testbot:8080/flash
 
+#curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data '{"ssid":"Wifi Connect","password":"password"}' \
+#   http://testbot:8080/connectAp/
+
 #should check whats returned
 exit 0
