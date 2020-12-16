811b6ea851c53d8c25d370c9e22cd91c696fb932
rcooke-warwick <ryan@balena.io>
2020-12-16 20:31:09 +0000

    connect to wifi added via bash
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- wifi-connect-exclave/logic/scripts/connect_to_wifi.bash
+++ wifi-connect-exclave/logic/scripts/connect_to_wifi.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-nmcli d wifi connect my_wifi password <password>
+#It takes time for wifi connect to become active - so we need to wait - or keep retrying if it fails
+
+nmcli d wifi connect "WiFi Connect"
 
 # https://core.docs.ubuntu.com/en/stacks/network/network-manager/docs/configure-wifi-connections
