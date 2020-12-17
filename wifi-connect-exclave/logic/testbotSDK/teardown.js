d2ce6728c359f4dd7218e31f5cf4a3f03896bbd6
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2020-12-17 14:57:39 +0530

    Add testbot teardown scripot
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- wifi-connect-exclave/logic/testbotSDK/teardown.js
+++ wifi-connect-exclave/logic/testbotSDK/teardown.js
@@ -1,13 +1,4 @@
-// Two approaches
-// 1. Create testbotSDK object here and flash the image
-// 2. Use Ryan's server.
-
-//  Do both.
-
-// Provision (flash) and Power on DUT (testbot-sdk)
-
 const { RaspberryPi, TestBotHat } = require('@balena/testbot')
-
 
 const testbotHat = new TestBotHat();
 const deviceInteractor = new RaspberryPi(testbotHat);
